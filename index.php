e84015712d125f64b9f1af48506c6d4c33073b75
Chaiaypong <mickey_mix@live.com>
2020-01-03 16:09:35 +0700

    actiomn

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
+<?php
+$action = isset($_GET['action']) ? $_GET['action'] : NULL;
+
+?>
+
+
 <html>
 <body>
 <div style="text-align: center;">
     <h3>WebView</h3>
-    <input type="text" id="textBox" ></br>
+    <input type="text" id="textBox" ><?php echo $action?></br>
     <button onclick="showMessage()">Show Message</button>
 </div>
 <script>
@@ -14,7 +20,7 @@
     }
 
     function showMessage() {
-        Android.showToast();
+        WebViewJS.onHeight();
     }
 </script>
 </body>
