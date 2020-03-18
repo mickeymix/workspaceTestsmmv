4bbd0e307ccb8817a44f650eb264f94d8facbfb1
Chaiaypong <mickey_mix@live.com>
2020-03-18 13:18:49 +0700

    change ajax post9

--- loginEncyption.php
+++ loginEncyption.php
@@ -12,23 +12,29 @@ if(isset($_POST['user'] , $_POST['pass'])){
 
 function oNatDncryption($action){
 
-    $password = 'fqJfdzGDvfwbedsKSUGty3VZ9taXxMVw';
+    $plaintext = 'My secret message 1234';
+    $password = '3sc3RLrpd17';
     $method = 'aes-256-cbc';
 
 // Must be exact 32 chars (256 bit)
     $password = substr(hash('sha256', $password, true), 0, 32);
-//    echo "Password:" . $password . "\n";
+    echo "Password:" . $password . "\n";
 
 // IV must be exact 16 chars (128 bit)
     $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
 
 // av3DYGLkwBsErphcyYp+imUW4QKs19hUnFyyYcXwURU=
-//$encrypted = base64_encode(openssl_encrypt($action, $method, $password, OPENSSL_RAW_DATA, $iv));
+    $encrypted = base64_encode(openssl_encrypt($plaintext, $method, $password, OPENSSL_RAW_DATA, $iv));
 
 // My secret message 1234
-    $decryption =  openssl_decrypt(base64_decode($action), $method, $password, OPENSSL_RAW_DATA, $iv);
-    echo $decryption;
-return $decryption;
+    $decrypted = openssl_decrypt(base64_decode($encrypted), $method, $password, OPENSSL_RAW_DATA, $iv);
+
+    echo 'plaintext=' . $plaintext . "\n";
+    echo 'cipher=' . $method . "\n";
+    echo 'encrypted to: ' . $encrypted . "\n";
+    echo 'decrypted to: ' . $decrypted . "\n\n";
+//    return $decrypted;
+
 }
 
 ?>
