c5097d9182f58333e88b13a337ccbc157dcf1086
Chaiaypong <mickey_mix@live.com>
2020-03-18 12:40:40 +0700

    change ajax post3

--- loginEncyption.php
+++ loginEncyption.php
@@ -1,13 +1,13 @@
 <?php
 
-
-//if(isset($_POST['user'] , $_POST['pass'])){
+echo $_POST["user"];
+if(isset($_POST['user'] , $_POST['pass'])){
 
     echo json_encode(array('status' => '1','message'=> "User Decryption is ".oNatDncryption($_POST['user'])." Pass Decryption is ".oNatDncryption($_POST['user']).""));
 
-//}else{
-//    echo json_encode(array('status' => '0','message'=> "Error insert data! "));
-//}
+}else{
+    echo json_encode(array('status' => '0','message'=> "Error insert data! "));
+}
 
 
 function oNatDncryption($action){
