f211b90dc50c0cbe1c7f7a5405279484b5fee07b
Chaiaypong <mickey_mix@live.com>
2020-03-26 17:18:49 +0700

    change ajax post25

--- parseEncryption.php
+++ parseEncryption.php
@@ -24,10 +24,11 @@ function oNatDecryption($keyType,$headerKey,$passwordValue){
 
     $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
 
-//    $keyForDecryption2 =  openssl_decrypt(base64_decode($headerKey), $method, $keyForEncryption, OPENSSL_RAW_DATA, $iv);
+    $keyForDecryption2 =  openssl_decrypt(base64_decode($headerKey), $method, $keyForEncryption, OPENSSL_RAW_DATA, $iv);
 
-    return openssl_decrypt(base64_decode($headerKey), $method, $keyForEncryption, OPENSSL_RAW_DATA, $iv);
-//    return  openssl_decrypt(base64_decode($passwordValue), $method, $keyForDecryption2, OPENSSL_RAW_DATA, $iv);
+    $keyForEncryptionHash2 = substr(hash('sha256', $keyForDecryption2, true), 0, 32);
+
+    return  openssl_decrypt(base64_decode($passwordValue), $method, $keyForEncryptionHash2, OPENSSL_RAW_DATA, $iv);
 }
 ?>
 
