743584fd8968cecfb7c482b454698c15f7f79a58
Chaiaypong <mickey_mix@live.com>
2020-03-26 20:46:39 +0700

    change ajax post28

--- createPasswordUsingKeyDynamic.php
+++ createPasswordUsingKeyDynamic.php
@@ -1,30 +1,52 @@
 <?php
 
 //echo $_POST["user"];
-if(isset($_POST['valuePlainText'],$_POST['key2'])){
+if(isset($_POST['valuePlainText'],$_POST['keytype'])){
 
-    echo json_encode(array('status' => '1','valueAfterEncryption'=> oNatEncryptionPassword($_POST['key2'],$_POST['valuePlainText'])));
+    $genKeyDynamic = generateKeyDynamic();
+
+    echo json_encode(array('status' => '1','resultGeneratedKey'=>$genKeyDynamic,'valueAfterEncryption'=> oNatEncryptionPassword($_POST['keytype'],$_POST['valuePlainText'],$genKeyDynamic)[0],'valueKeyEncryption'=> oNatEncryptionPassword($_POST['keytype'],$_POST['valuePlainText'],$genKeyDynamic)[1]));
 
 }else{
     echo json_encode(array('status' => '0','message'=> "Error insert data! "));
 }
 
+function generateKeyDynamic(){
+    $myDateTime =  DateTime::createFromFormat('yyyyMMddhhmmss', Date());
+    return "abc'".$myDateTime."'".generateRandomString(12)."'";
 
-
-function oNatEncryptionPassword($keyEncryption2, $passwordPlaneText)
+}
+function generateRandomString($length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+function oNatEncryptionPassword($keyType, $passwordPlaneText,$genKeyDynamic)
 {
-    $password = $keyEncryption2;
-
+//    $password = $keyEncryption2;
+    if (0 === $keyType){
+        $password = 'gv[u:ugvHogvmuF,[kpcvr]bg8=yjo20';
+    }else{
+        $password = 'gv[u:uc=mcvofNgmiflesiy[gfaot0Ut';
+    }
     $method = 'aes-256-cbc';
 
-// Must be exact 32 chars (256 bit)
-    $password = substr(hash('sha256', $password, true), 0, 32);
-//    echo "Password:" . $password . "\n";
+    $hashKeyOne = substr(hash('sha256', $password, true), 0, 32);
+
+    $hashKeyTwo = substr(hash('sha256', $genKeyDynamic, true), 0, 32);
 
 // IV must be exact 16 chars (128 bit)
     $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
 
-    return base64_encode(openssl_encrypt($passwordPlaneText, $method, $password, OPENSSL_RAW_DATA, $iv));
+    $encryptValue = base64_encode(openssl_encrypt($passwordPlaneText, $method, $hashKeyTwo, OPENSSL_RAW_DATA, $iv));
+
+    $encryptKeyTwo = base64_encode(openssl_encrypt($genKeyDynamic, $method, $hashKeyOne, OPENSSL_RAW_DATA, $iv));
+
+    return array($encryptValue,$encryptKeyTwo);
 
 }
 
