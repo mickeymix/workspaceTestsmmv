d6b14188945dbc56c217e6c83d9e721ae35be969
Chaiaypong <mickey_mix@live.com>
2020-03-26 21:01:31 +0700

    change ajax post31

--- createPasswordUsingKeyDynamic.php
+++ createPasswordUsingKeyDynamic.php
@@ -13,7 +13,7 @@ if(isset($_POST['valuePlainText'],$_POST['keytype'])){
 
 function generateKeyDynamic(){
     $myDateTime =  DateTime::createFromFormat('yyyyMMddhhmmss', Date());
-    return "abc".date('YMdhis')."".generateRandomString(12)."nat";
+    return "abc".date('YmdHis')."".generateRandomString(12)."nat";
 
 }
 function generateRandomString($length) {
