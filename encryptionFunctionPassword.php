5200f1c6edea9718bd1ac6c317385f14b7db4027
Chaiaypong <mickey_mix@live.com>
2020-03-31 10:45:25 +0700

    fix encrytion function

--- encryptionFunctionPassword.php
+++ encryptionFunctionPassword.php
@@ -1,19 +1,26 @@
 <?php
-function oNatEncryptionPassword($keyEncryption2, $passwordPlaneText)
+function oNatEncryptionPassword($keyType, $passwordPlaneText,$genKeyDynamic)
 {
-    $password = $keyEncryption2;
-
+//    $password = $keyEncryption2;
+    if (0 === $keyType){
+        $password = 'gv[u:ugvHogvmuF,[kpcvr]bg8=yjo20';
+    }else{
+        $password = 'gv[u:uc=mcvofNgmiflesiy[gfaot0Ut';
+    }
     $method = 'aes-256-cbc';
 
-// Must be exact 32 chars (256 bit)
-    $password = substr(hash('sha256', $password, true), 0, 32);
-//    echo "Password:" . $password . "\n";
+    $hashKeyOne = substr(hash('sha256', $password, true), 0, 32);
+
+    $hashKeyTwo = substr(hash('sha256', $genKeyDynamic, true), 0, 32);
 
 // IV must be exact 16 chars (128 bit)
     $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
 
-    return base64_encode(openssl_encrypt($passwordPlaneText, $method, $password, OPENSSL_RAW_DATA, $iv));
+    $encryptValue = base64_encode(openssl_encrypt($passwordPlaneText, $method, $hashKeyTwo, OPENSSL_RAW_DATA, $iv));
+
+    $encryptKeyTwo = base64_encode(openssl_encrypt($genKeyDynamic, $method, $hashKeyOne, OPENSSL_RAW_DATA, $iv));
+
+    return array($encryptValue,$encryptKeyTwo);
 
 }
-
 ?>
